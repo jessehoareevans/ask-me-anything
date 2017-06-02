d93cbb734ab9ba7c7be79b6f478934003484bc12
Jesse Hoare Evans <jesse.hoareevans@gmail.com>
2017-06-02 11:30:52 -0700

    add basic functionality to update and create a new question

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -9,6 +9,9 @@ export default Ember.Component.extend({
     notesHide: function() {
       this.set('isNotesShowing', false);
     },
+    update(question, params) {
+      this.sendAction('update', question, params);
+    },
     delete(question) {
       if (confirm('Are you sure you want to delete this question?')) {
         this.sendAction('destroyQuestion', question);
